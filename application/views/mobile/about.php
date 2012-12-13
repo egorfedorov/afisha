87c0d45a5e7accd53eda0a211a640ee442dbe798
Dima Tepalenko <tepalenko@gmail.com>
2012-12-13 20:54:27 +0200

    sadf

---
+++ application/views/mobile/about.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <h3 class="left-text">Мы на связе 24 часа в сутки, свяжитесь с нами</h3>
+    <p class="left-text">Всегда ответим на ваши вопросы</p>
+</div>
+
+<div class="decoration"></div>
+
+<div class="container">
+    <div id="gallery2" class="gallery2">
+
+            <div>
+                <!--a href="<?=$event['main_img']?>">
+                    <img src="<?=$event['main_img']?>" alt="<?=$event['title']?>" class="event_img"/>
+                </a-->
+                <h3 class="gallery-title left-text">Freetime.ck.ua - сайт для активных людей</h3>
+
+                <p class="gallery-description left-text">
+                <p><b>Команда нашего сайта работает круглосуточно что бы вы были в курсе всех событий в Черкассах</b></p>
+                <p class="separator_2">&nbsp;</p>
+                <p>Кинотеатры, вечеринки все рассписания только у нас. Свяжитесь с нами</p>
+
+                </p>
+
+                <div style="clear: both;"></div>
+            </div>
+            <div class="decoration">
+                <div class="deco1"></div>
+                <div class="deco2"></div>
+            </div>
+
+
+    </div>
+</div>
